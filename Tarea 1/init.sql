368f66d5cb15ee8d4a6081fdfbe0b477e5ab64e1
aleudp <163481252+aleudp@users.noreply.github.com>
2024-09-14 13:04:14 -0300

    Delete Tarea 1 directory

--- Tarea 1/init.sql
+++
@@ -1,8 +0,0 @@
--- init.sql
-CREATE TABLE IF NOT EXISTS dns_queries (
-    id SERIAL PRIMARY KEY,
-    domain VARCHAR(255),
-    ip_address VARCHAR(255),
-    query_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
